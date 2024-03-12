6aa0a27cee46c6db3c8dc7e92e95dfe6040dc46e
karanchandwani12 <karanchandwanigg@gmail.com>
2024-03-12 19:44:21 +0530

    Css and HTML added

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -2,6 +2,7 @@ import { Component, SimpleChange } from '@angular/core';
 import { SjobServService } from '../sjob-serv.service';
 import { IJobDetail } from '../ijob-detail';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
@@ -14,10 +15,14 @@ export class FavoritesComponent {
   public myfavMarkedJobs= this.myServ.favMarkedJobs;
   public jobs: Array<IJobDetail>=[];
 
-  constructor(private myServ : SjobServService){}
+  constructor(private myServ : SjobServService, private route : Router){}
  
   ngOnInit(){
      this.myServ.getJobDetails().subscribe(data=>this.jobs=data);
   }
 
+  jobdesc( job: IJobDetail){
+    // this.jobServ.descibeJob=job.description;
+     this.route.navigate(['/jobs',job.id])
+   }
 }
